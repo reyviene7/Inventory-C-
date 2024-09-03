45bc7968e2a3c24ff4934197c841aa974a3806c7
Rey Viene Dean Pasiculan <reyvienepasiculan7@gmail.com>
2024-09-04 00:13:07 +0800

    feat: added sales particular report to form management

---
+++ Inventory/Class/SalesParticularList.cs
@@ -0,0 +1,20 @@
+﻿ 
+
+using System;
+
+namespace Inventory.Class
+{
+    public class SalesParticularList
+    {
+        public int      Id          { get; set; }
+        public string Item { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public decimal Gross { get; set; }
+        public decimal Net { get; set; }
+        public string Branch { get; set; }
+        public string Customer { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Inventory/Interface/ISalesParticular.cs
@@ -0,0 +1,13 @@
+﻿ 
+
+using System;
+using System.Collections.Generic;
+using Inventory.Entities;
+
+namespace Inventory.Interface
+{
+    public interface ISalesParticular
+    {
+        IEnumerable<ViewReportSalesParticular> SalesParticular(DateTime startDate, DateTime endDate);
+    }
+}
