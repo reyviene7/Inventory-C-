502f5e71bb45f1c9a1893ac43d34f6533fbc2a70
Rey Viene Dean Pasiculan <reyvienepasiculan7@gmail.com>
2025-08-05 22:46:56 +0800

    refactor: fixed products bugs

--- Inventory/MainForm/FirmProducts.cs
+++ Inventory/MainForm/FirmProducts.cs
@@ -39,13 +39,16 @@ namespace Inventory.MainForm
             _userId = userId;
             _userTyp = userTy;
 
-            if (_userTyp != 1)
+            var allowedRoles = new List<int> { 1, 2 }; // Admin and User
+
+            if (!allowedRoles.Contains(_userTyp))
             {
                 PopupNotification.PopUpMessages(0, Messages.AdminPrivilege, Messages.InventorySystem);
 
                 this.DialogResult = DialogResult.Cancel;
                 return;
             }
+
             InitializeComponent();
             this.DialogResult = DialogResult.OK;
         }
