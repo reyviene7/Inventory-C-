bb998e391e807f7dfb7a41b265237346048f2354
Rey Viene Dean Pasiculan <reyvienepasiculan7@gmail.com>
2025-09-07 14:35:26 +0800

    refactor: modified splash screen for better UI

--- Inventory/MainForm/WaitForm1.cs
+++ Inventory/MainForm/WaitForm1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -14,7 +15,11 @@ namespace Inventory.MainForm
     {
         public WaitForm1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+            this.DoubleBuffered = true;
         }
 
         #region Overrides
@@ -23,6 +28,50 @@ namespace Inventory.MainForm
         {
             base.SetCaption(caption);
             labelTitle.Text = caption;
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            int radius = 25; // Adjust for smoother roundness
+            using (GraphicsPath path = GetRoundedPath(new Rectangle(0, 0, this.Width, this.Height), radius))
+            {
+                this.Region = new Region(path);
+            }
+            this.Invalidate();
+        }
+
+        private GraphicsPath GetRoundedPath(Rectangle rect, int radius)
+        {
+            float r2 = radius * 2f;
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+
+            // Top-left corner
+            path.AddArc(rect.X, rect.Y, r2, r2, 180, 90);
+
+            // Top edge
+            path.AddLine(rect.X + radius, rect.Y, rect.Right - radius, rect.Y);
+
+            // Top-right corner
+            path.AddArc(rect.Right - r2, rect.Y, r2, r2, 270, 90);
+
+            // Right edge
+            path.AddLine(rect.Right, rect.Y + radius, rect.Right, rect.Bottom - radius);
+
+            // Bottom-right corner
+            path.AddArc(rect.Right - r2, rect.Bottom - r2, r2, r2, 0, 90);
+
+            // Bottom edge
+            path.AddLine(rect.Right - radius, rect.Bottom, rect.X + radius, rect.Bottom);
+
+            // Bottom-left corner
+            path.AddArc(rect.X, rect.Bottom - r2, r2, r2, 90, 90);
+
+            // Left edge
+            path.AddLine(rect.X, rect.Bottom - radius, rect.X, rect.Y + radius);
+
+            path.CloseFigure();
+            return path;
         }
         public override void SetDescription(string description)
         {
@@ -36,12 +85,8 @@ namespace Inventory.MainForm
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias; // smooth edges
         }
-
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-        private static extern IntPtr CreateRoundRectRgn(int nLeft, int nTop, int nRight, int nBottom, int widthEllipse, int heightEllipse);
-
         #endregion
 
         public enum WaitFormCommand
